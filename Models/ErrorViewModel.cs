0e7f82e34aa83728b1dfb9b0697857e4ff0c08f1
ElPuH0 <ElPuH0@hotmail.com>
2021-08-11 18:02:08 -0500

    make it better

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lavanderia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
